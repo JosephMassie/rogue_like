fe77f0ed028013afcade574f631af12246c925d2
Joseph Massie <josephmmassie@gmail.com>
2024-11-11 18:35:59 -0800

    Created a new Int_Vector class to create simple 2d vectors that, outside   of getting their magnitude, stick strictly to using ints for eas of use   with the terminal grid
    Refactor World, Terminal, TermSprite to all use this new Int_Vector
      for positions
    
    Created a new Player class to store all updates and actions related
      to the player character
    
    Created a new Keyboard class to centralize all handling of pygame
      keyboard based events, Also switch from using pygame's key.getPressed
      method to instead use key events directly to tigher control
    
    Moved all global constants to a centralized contants.py file

---
+++ src/entities/player.py
@@ -0,0 +1,45 @@
+import pygame
+
+from utils.constants import *
+
+from term import Terminal, TermSprite, TermContent
+from world import *
+from utils.input import Keyboard
+from utils.int_vect import Int_Vector
+
+class Player():
+    def __init__(self, position: Int_Vector, term: Terminal, level: World, keyboard: Keyboard) -> None:
+        pcon: TermContent = {
+            "chars": [["@"]],
+            "colors": [[(DEFAULT_BG_COLOR, pygame.Color(0, 150, 0))]]
+        }
+        self._term = term
+        self._level = level
+        self._position = position
+        self._keyboard  = keyboard
+        screen_pos = self._level.world_coord_to_screen_cord(position)
+        self._sprite = TermSprite(pcon, screen_pos)
+    
+    def render(self):
+        screen_pos = self._level.world_coord_to_screen_cord(self._position)
+        self._sprite.setPosition(screen_pos)
+        self._term.draw_term_sprite(self._sprite)
+    
+    def set_position(self, position: Int_Vector):
+        self._position = position
+    
+    def move(self, delta: Int_Vector):
+        self._position = self._position + delta
+    
+    def update(self) -> None:
+        directions = [
+            (pygame.K_UP, (0, -1)),
+            (pygame.K_DOWN, (0, 1)),
+            (pygame.K_LEFT, (-1, 0)),
+            (pygame.K_RIGHT, (1, 0))
+        ]
+
+        for key, dir in directions:
+            if self._keyboard.pressed(key):
+                delta = Int_Vector(*dir)
+                self.move(delta)
--- src/world.py
+++ src/world.py
@@ -1,10 +1,18 @@
 import os
 
 from term import *
+from utils.int_vect import Int_Vector
+from utils.input import Keyboard
 
 class World():
-    def __init__(self, filePath: str, term: Terminal) -> None:
+    def __init__(
+            self,
+            filePath: str,
+            term: Terminal,
+            keyboard: Keyboard,
+            ) -> None:
         self._term = term
+        self._keyboard = keyboard
         with open(filePath) as file:
             data = file.read()
             data = data.splitlines()
@@ -12,8 +20,7 @@ class World():
             self._grid: TermCharBuff = []
             for line in data:
                 self._grid.append(list(line))
-        self._camera_x = 0
-        self._camera_y = 0
+        self._camera_pos = Int_Vector(0, 0)
     
     def _cut_to_camera(self) -> TermCharBuff:
         screen_width = self._term.get_width()
@@ -23,24 +30,39 @@ class World():
         grid_max_height = len(self._grid)
         grid_max_width = get_char_buff_width(self._grid)
 
-        buff_height = self._camera_y + screen_height
+        cam_x, cam_y = self._camera_pos.getCoords()
+        buff_height = cam_y + screen_height
         if buff_height > grid_max_height:
             buff_height = grid_max_height
-        buff_width = self._camera_x + screen_width
+        buff_width = cam_x + screen_width
         if buff_width > grid_max_width:
             buff_width = grid_max_width
 
-        for i in range(self._camera_y, buff_height):
-            grid.append(self._grid[i][self._camera_x:buff_width])
+        for i in range(cam_y, buff_height):
+            grid.append(self._grid[i][cam_x:buff_width])
         return grid
 
-    def move_camera(self, x: int, y: int) -> None:
-        self._camera_x += x
-        if self._camera_x < 0:
-            self._camera_x = 0
-        self._camera_y += y
-        if self._camera_y < 0:
-            self._camera_y = 0
+    def move_camera(self, direction: Int_Vector) -> None:
+        self._camera_pos = self._camera_pos + direction
+        x, y = self._camera_pos.getCoords()
+        if x < 0:
+            x = 0
+        if y < 0:
+            y = 0
+        self._camera_pos.setCoords((x, y))
+    
+    def update(self) -> None:
+        directions = [
+            (pygame.K_w, (0, -1)),
+            (pygame.K_a, (-1, 0)),
+            (pygame.K_s, (0, 1)),
+            (pygame.K_d, (1, 0))
+        ]
+
+        for key, dir in directions:
+            if self._keyboard.pressed(key):
+                delta = Int_Vector(*dir)
+                self.move_camera(delta)
     
     def render(self) -> None:
         frame: TermContent = {
@@ -48,5 +70,7 @@ class World():
         }
         self._term.draw_element(frame, 0, 0)
     
-    def world_coord_to_screen_cord(self, x: int, y: int) -> tuple[int, int]:
-        return x - self._camera_x, y - self._camera_y
+    def world_coord_to_screen_cord(self, position: Int_Vector) -> Int_Vector:
+        spos = position - self._camera_pos
+        #print(f"{position} - {self._camera_pos} = {spos}")
+        return spos
